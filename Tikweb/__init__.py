11cbb024174e23e4f77b027a6f550313dfaa5387
Trade999 <saf81181@gmail.com>
2025-09-15 15:15:25 +0300

    Update __init__.py

--- Tikweb/__init__.py
+++ Tikweb/__init__.py
@@ -1,5 +1,2 @@
 from .client import TikClient
-from .exceptions import PasswordError, FileError, PError, connectionError, detection, loginRequired
 from utils.x_gnarly import get_x_gnarly
-
-__all__ = ["TikClient", "PasswordError", "FileError", "PError", "connectionError", "detection", "loginRequired", "get_x_gnarly"]
