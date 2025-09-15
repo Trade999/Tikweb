11e814961b9560b6a468584df6b6b885d199c63b
Trade999 <saf81181@gmail.com>
2025-09-15 14:21:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="Tikweb",
-    version="1.0.0",
+    version="1.0.2",
     description="TikTok Web",
     author="Lariot",
     author_email="lariot.antsa@gmail.com",
