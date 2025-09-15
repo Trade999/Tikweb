575abf9712db2001d96e5cb29aa765244d8faf0d
Trade999 <saf81181@gmail.com>
2025-09-15 13:30:04 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name="Tikweb",
+    version="1.0.0",
+    description="TikTok Web",
+    author="Lariot",
+    author_email="lariot.antsa@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "pyexecjs",
+    ],
+    entry_points={
+        'console_scripts': [
+            'report-tiktok=TikTok.main:main'
+        ]
+    },
+    python_requires=">=3.6",
+)
