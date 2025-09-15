a7c16b0c723b67eaadb378646f68888755e67f8b
Trade999 <saf81181@gmail.com>
2025-09-15 15:21:25 +0300

    Add files via upload

---
+++ Tikweb/__init__.py
@@ -0,0 +1,3 @@
+from .client import TikClient, PasswordError, FileError, PError, connectionError, detection, loginRequired
+
+__all__ = ["TikClient", "PasswordError", "FileError", "PError", "connectionError", "detection", "loginRequired"]
