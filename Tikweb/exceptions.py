cfda53c7a7f4bccec82b0ecf1c0beb1bf1f8a727
Trade999 <saf81181@gmail.com>
2025-09-15 13:57:17 +0300

    Add files via upload

---
+++ Tikweb/exceptions.py
@@ -0,0 +1,12 @@
+class FileError(Exception):
+  pass
+class PError(Exception):
+  pass
+class connectionError(Exception):
+  pass
+class PasswordError(Exception):
+  pass
+class detection(Exception):
+  pass
+class loginRequired(Exception):
+  pass
